881b11a5f953cae493b17f61a6c6192fb5a398cf
dolabelag <dolabelag@gmail.com>
2025-08-10 17:32:09 -0300

    Add exception classes to application and infrastructure

---
+++ Codigo/src/hookci/application/errors.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Copyright (C) 2025 PUC Minas, Henrique Almeida, Gabriel Dolabela
+# This file is part of HookCI.
+
+# HookCI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# HookCI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with HookCI.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+Custom exceptions for the application layer.
+"""
+
+class ApplicationError(Exception):
+    """Base class for application-related errors."""
+    pass
+
+class ProjectAlreadyInitializedError(ApplicationError):
+    """Raised when trying to initialize an already initialized project."""
+    pass
