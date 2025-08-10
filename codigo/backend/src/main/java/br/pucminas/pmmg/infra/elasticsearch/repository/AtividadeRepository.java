0fc8def69901213144d4aadeda0b83d955ee73ad
Tabosaaa <tabosalucas2011@gmail.com>
2025-08-10 12:47:13 -0300

    Excluir arquivos não utilizados

--- codigo/backend/src/main/java/br/pucminas/pmmg/infra/elasticsearch/repository/AtividadeRepository.java
+++
@@ -1,9 +0,0 @@
-package br.pucminas.pmmg.infra.elasticsearch.repository;
-
-import br.pucminas.pmmg.infra.elasticsearch.entity.Atividade;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import java.util.UUID;
-
-public interface AtividadeRepository extends ElasticsearchRepository<Atividade, UUID> {
-}
