9de65c2a7c447d9f8134844c696837d23ce98323
h3nc4 <me@h3nc4.com>
2025-10-11 10:17:32 -0300

    publish app via CI

---
+++ scripts/wasm-deps.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Copyright (C) 2025  Henrique Almeida
+# This file is part of WASudoku.
+#
+# WASudoku is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# WASudoku is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with WASudoku.  If not, see <https://www.gnu.org/licenses/>.
+
+set -e
+
+tools="wasm-pack 0.13.1"
+
+echo "$tools" | while read -r name ver; do
+  if ! command -v "$name" >/dev/null 2>&1; then
+    echo "installing $name $ver"
+    cargo install "$name" --version "$ver" --locked
+  fi
+done
--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/WASudoku/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
